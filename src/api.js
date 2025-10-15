6f0cc0150ff8a1029cee26011a715074b7c3ae22
rishbahjain270804 <rishabhjain27082004@gmail.com>
2025-10-16 01:54:13 +0530

    Switch to test keys and localhost for testing - Frontend

--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-export const API_BASE = import.meta.env.VITE_API_BASE_URL || 'https://farmformback.onrender.com'
-export const RAZORPAY_KEY_ID = import.meta.env.VITE_RAZORPAY_KEY_ID || 'rzp_live_RT3fwUOLsjptba'
-export const RAZORPAY_KEY_SECRET = import.meta.env.VITE_RAZORPAY_KEY_SECRET || 'DgXEOyCIrisBmePTZZwdibcR'
+export const API_BASE = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080'
+export const RAZORPAY_KEY_ID = import.meta.env.VITE_RAZORPAY_KEY_ID || 'rzp_test_RTsRWiZCfVmrgI'
+export const RAZORPAY_KEY_SECRET = import.meta.env.VITE_RAZORPAY_KEY_SECRET || '7kelrOCQrsbW7c6e6GOduLAy'
 
 export const api = axios.create({
   baseURL: API_BASE,
